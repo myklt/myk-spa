4c0f702110a603f31cea405e68dec0359034b565
myklt <info@myk.lt>
2019-03-26 00:34:37 +0100

    Updates

---
+++ precache-manifest.1b8b69739929bee0433cc3c7e27915a7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "37d263a5e3df3e28917d",
+    "url": "/static/js/main.95b5f3fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1eb804cfda2f4d08eb42",
+    "url": "/static/js/2.f7d7609c.chunk.js"
+  },
+  {
+    "revision": "da133bee5862010cfeee4eb1833736df",
+    "url": "/index.html"
+  }
+];
