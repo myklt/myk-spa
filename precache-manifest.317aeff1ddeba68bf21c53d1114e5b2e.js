6f5f486fdd64d65fa2328548cea7c75dd7d9e47a
myklt <info@myk.lt>
2019-05-11 13:03:09 +0200

    Updates

---
+++ precache-manifest.317aeff1ddeba68bf21c53d1114e5b2e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ab4afeb41dc9138dc46e752f342900",
+    "url": "/myk-spa/index.html"
+  },
+  {
+    "revision": "021335def4bd972c610a",
+    "url": "/myk-spa/static/js/2.ba0452ba.chunk.js"
+  },
+  {
+    "revision": "692def86067a63099822",
+    "url": "/myk-spa/static/js/main.8a332b09.chunk.js"
+  },
+  {
+    "revision": "8cc80adf736cb0c6ff91",
+    "url": "/myk-spa/static/js/runtime~main.085c5ec4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myk-spa/precache-manifest.4ceb289e4c19c33cef16fcc2bb3b2e35.js"
+  "/myk-spa/precache-manifest.317aeff1ddeba68bf21c53d1114e5b2e.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myk-spa/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myk-spa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
