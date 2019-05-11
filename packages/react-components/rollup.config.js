da7194474eaf571aab4aace3aa6dedcd6f8c193a
myklt <info@myk.lt>
2019-05-11 12:16:04 +0200

    Rename non standard extensions
    Remove once https://github.com/rollup/rollup/issues/2842 is resolved

--- packages/react-components/rollup.config.js
+++ packages/react-components/rollup.config.js
@@ -2,6 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
+import renameExtensions from '@betit/rollup-plugin-rename-extensions';
 import path from 'path';
 import pkg from './package.json';
 
@@ -10,6 +11,14 @@ export default [
     input: 'index.ts',
     external: Object.keys(pkg.peerDependencies || {}),
     plugins: [
+      renameExtensions({
+        include: ['**/*.tsx', '**/*.ts', '**/*.scss'],
+        mappings: {
+          '.tsx': '.js',
+          '.ts': '.js',
+          '.scss': '.styles.js',
+        },
+      }),
       resolve(),
       commonjs(),
       typescript({
